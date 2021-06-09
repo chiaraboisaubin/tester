5c7586c2f9d56c663027f91e4f78d0d53ae576ad
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2021-06-08 22:49:15 -0400

    Updates

---
+++ precache-manifest.3d5d8b61ae7a0bbf0a7e417aa5c5b9e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca39f09237623af0eb22b615f0024275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1df31e1e9766a85041c",
+    "url": "/static/css/2.12f074cd.chunk.css"
+  },
+  {
+    "revision": "c2d562c649dd52648093",
+    "url": "/static/css/main.0a89cd1e.chunk.css"
+  },
+  {
+    "revision": "c1df31e1e9766a85041c",
+    "url": "/static/js/2.c62a0ce1.chunk.js"
+  },
+  {
+    "revision": "1b00c1476c5ada843f3d47ab49855ed5",
+    "url": "/static/js/2.c62a0ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d562c649dd52648093",
+    "url": "/static/js/main.39031621.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b46a1204b2b02fe9c331f67a7f076f25.js"
+  "/precache-manifest.3d5d8b61ae7a0bbf0a7e417aa5c5b9e6.js"
 );
 
 self.addEventListener('message', (event) => {
